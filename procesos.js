f9da69a29521f39adbb4b0a8f9199397ae0208b5
ricardo-julio <138219637+ricardo-julio@users.noreply.github.com>
2023-06-30 07:43:05 -0500

    first commit

---
+++ procesos.js
@@ -0,0 +1,13 @@
+function pia() {
+document.getElementById("pio").innerHTML = ""
++"<p>"
+        
++"La inauguración del Mundial 2014 se llevó a cabo el 12 de junio de 2014 en el Estadio Arena Corinthians en São Paulo, Brasil. Fue un evento espectacular que marcó el comienzo de la Copa del Mundo de la FIFA 2014."
+
++"La ceremonia de apertura contó con una mezcla de música, danza y exhibiciones artísticas. Hubo actuaciones de renombrados artistas brasileños, como Claudia Leitte, Jennifer Lopez y Pitbull, quienes interpretaron el himno oficial de la Copa del Mundo titulado We Are One (Ole Ola)."
+
++"El punto culminante de la ceremonia fue el tradicional saque inicial del torneo, realizado por un niño discapacitado llamado Brazuca, que era el nombre del balón oficial del Mundial. Después del saque inicial, se llevó a cabo el primer partido del torneo entre las selecciones de Brasil y Croacia."
+
++"A pesar de algunas protestas y problemas organizativos previos al evento, la inauguración del Mundial 2014 fue un gran espectáculo que atrajo la atención de millones de personas en todo el mundo y marcó el inicio de un mes emocionante de fútbol en Brasil.";
+    +"</p>"
+}
